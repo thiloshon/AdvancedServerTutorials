816b616b45c953df352422ef5a4e67e123a078fd
Thiloshon <thiloshon@gmail.com>
2018-11-03 19:31:14 +0530

    End of Week 02 / Tutorial 02

--- Week02/application/controllers/products.php
+++ Week02/application/controllers/products.php
@@ -8,8 +8,29 @@
  */
 class Products extends CI_Controller
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('product_model');
+    }
+
     public function index(){
         $this->load->view('product_lookup');
     }
 
+    function find(){
+        // Methods to access GET
+        //echo $_GET["PRODUCT_TYPE"];
+        // echo $this->input->get('PRODUCT_TYPE');
+        $type = $this->input->get('PRODUCT_TYPE');
+
+        //Here or at constructor
+        //$this->load->model('product_model');
+        $products = $this->product_model->lookup($type);
+
+
+        $answer['products'] = $products;
+        $this->load->view('product_display', $answer);
+    }
 }
---
+++ Week02/application/views/product_display.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thiloshon
+ * Date: 03-Nov-18
+ * Time: 7:12 PM
+ */
+
+foreach ($products as $value) {
+    echo "$value </br>";
+}
