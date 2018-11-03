71ecfa0436a8519bccc8fc04c7737c428461d0b1
Thiloshon <thiloshon@gmail.com>
2018-11-03 19:33:17 +0530

    Adding Week 03 / Tutorial 02

---
+++ Week03/application/views/product_lookup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thiloshon
+ * Date: 01-Nov-18
+ * Time: 8:01 PM
+ */
+
+?>
+
+<form action="http://localhost/AdvancedServerTutorials/Week02/index.php/products/find">
+    <input type="text" name='PRODUCT_TYPE'>
+    Type of the Product
+    <button type="submit">Send</button>
+</form>
